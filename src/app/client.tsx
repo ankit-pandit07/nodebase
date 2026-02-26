42792415671b5f24fb4763208fc303fc4b2dc28e
ankit-pandit07 <panditankit865@gmail.com>
2026-02-26 23:06:01 +0530

    setup better-auth

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useTRPC } from "@/trpc/Client"
+import { useTRPC } from "@/trpc/client"
 import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const Client=()=>{
