ae900b8099a7225b7aa082e06d391d4f62b88d0d
ankit-pandit07 <panditankit865@gmail.com>
2026-03-05 14:46:20 +0530

    workflows_crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,27 @@
 import { requireAuth } from "@/lib/auth-utlis";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+import { Suspense } from "react";
+import { WorkflowsContainer, WorkflowsList } from "@/features/workflows/components/workflows";
+
 
 const Page=async()=>{
     await requireAuth();
+
+    prefetchWorkflows();
     
-    return <p>Workflows</p>
+    return (
+        <WorkflowsContainer>
+        <HydrateClient>
+            <ErrorBoundary fallback={<p>Error!</p>}>
+            <Suspense fallback={<p>Loading...</p>}>
+            <WorkflowsList />
+            </Suspense>
+            </ErrorBoundary>
+        </HydrateClient>
+        </WorkflowsContainer>
+    )
 }
 
 export default Page;
