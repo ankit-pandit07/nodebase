5015f6f350f84e18fc0d506240c63975a9d710f7
ankit-pandit07 <panditankit865@gmail.com>
2026-03-07 21:33:55 +0530

    added editor using react_flow

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -8,20 +8,14 @@ import { ReactFlow, applyEdgeChanges, applyNodeChanges, addEdge,
   type Connection,
   Background,
   Controls,
-  MiniMap} from "@xyflow/react"
+  MiniMap,
+  Panel} from "@xyflow/react"
 import '@xyflow/react/dist/style.css';
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+import { nodeComponents } from "@/config/node-components";
+import { AddNodeButton } from "./add-node-button";
 
-const initialNodes = [
-  { id: 'n1', position: { x: 0, y: 0 },
-   data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 },
-   data: { label: 'Node 2' } },
-];
-
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
- 
 export const EditorLoading = () => {
   return <LoadingView message="Loading editor..." />
 }
@@ -31,8 +25,8 @@ export const EditorError = () => {
 
 export const Editor = (({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseWorkflow(workflowId);
-  const [nodes, setNodes] = useState<Node[]>(initialNodes);
-  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+  const [nodes, setNodes] = useState<Node[]>(workflow.nodes);
+  const [edges, setEdges] = useState<Edge[]>(workflow.edges);
 
     const onNodesChange = useCallback(
     (changes:NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
@@ -55,12 +49,16 @@ export const Editor = (({ workflowId }: { workflowId: string }) => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
+        nodeTypes={nodeComponents}
         fitView
        
       >
         <Background />
         <Controls />
         <MiniMap />
+        <Panel position="top-right">
+          <AddNodeButton/>
+        </Panel>
         </ReactFlow>
     </div>
   );
