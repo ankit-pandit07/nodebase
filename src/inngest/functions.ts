4f7a060c05dfad55ae127f5a87e724a9c3f24dff
ankit-pandit07 <panditankit865@gmail.com>
2026-03-15 14:43:03 +0530

    added discord node

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -10,6 +10,7 @@ import { manualTriggerChannel } from "./channels/manual-trigger";
 import { googleFormTriggerChannel } from "./channels/google-form-trigger";
 import { stripeTriggerChannel } from "./channels/stripe-trigger";
 import { geminiChannel } from "./channels/gemini";
+import { discordChannel } from "./channels/discord";
 
 export const executeWorkflow=inngest.createFunction(
     {
@@ -23,7 +24,8 @@ export const executeWorkflow=inngest.createFunction(
             manualTriggerChannel(),
             googleFormTriggerChannel(),
             stripeTriggerChannel(),
-            geminiChannel()
+            geminiChannel(),
+            discordChannel()
         ]
     },
     async({event, step, publish})=>{
@@ -43,6 +45,16 @@ export const executeWorkflow=inngest.createFunction(
             return topologicalSort(workflow.nodes, workflow.connections);
         });
 
+        const userId=await step.run("find-user-id", async()=>{
+            const workflow=await prisma.workflow.findUniqueOrThrow({
+                where:{id:workflowId},
+                select:{
+                    userId:true
+                }
+            });
+            return workflow.userId
+        })
+
         // Initialize context with any initial data from the trigger
         let context=event.data.initialData  || {};
 
@@ -52,6 +64,7 @@ export const executeWorkflow=inngest.createFunction(
             context=await executor({
                 data:node.data as Record<string,unknown>,
                 nodeId:node.id,
+                userId,
                 context,
                 step,
                 publish,
