7362d5e894356abf50eae3b81bdd90ad5d5b7099
ankit-pandit07 <panditankit865@gmail.com>
2026-03-15 22:30:59 +0530

    added execution history

--- src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
+++ src/app/(dashboard)/(rest)/executions/[executionId]/page.tsx
@@ -1,4 +1,11 @@
+
+import { ExecutionView } from "@/features/executions/components/execution";
+import { ExecutionsError, ExecutionsLoading } from "@/features/executions/components/executions";
+import { prefetchExecution } from "@/features/executions/server/prefetch";
 import { requireAuth } from "@/lib/auth-utlis";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
 
 interface PageProps{
     params:Promise<{
@@ -7,8 +14,24 @@ interface PageProps{
 }
 const Page=async({params}:PageProps)=>{
     await requireAuth();
+
     const {executionId}=await params;
-    return <p>Execution Id: {executionId}</p>
+    prefetchExecution(executionId);
+
+    return (
+        <div className="p-4 md:px-10 md:py-6 h-full">
+            <div className="mx-auto max-w-screen-md w-full flex flex-col
+            gap-y-8 h-full">
+                <HydrateClient>
+                    <ErrorBoundary fallback={<ExecutionsError />}>
+                    <Suspense fallback={<ExecutionsLoading />}>
+                        <ExecutionView executionId={executionId}/>
+                    </Suspense>
+                    </ErrorBoundary>
+                </HydrateClient>
+            </div>
+        </div>
+    )
 }
 
 export default Page;
--- src/app/(dashboard)/(rest)/executions/page.tsx
+++ src/app/(dashboard)/(rest)/executions/page.tsx
@@ -1,8 +1,32 @@
 import { requireAuth } from "@/lib/auth-utlis";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { SearchParams } from "nuqs";
+import { Suspense } from "react";
+import { executionsParamsLoader } from "@/features/executions/server/params-loader";
+import { prefetchExecutions } from "@/features/executions/server/prefetch";
+import { ExecutionsContainer, ExecutionsError, ExecutionsList, ExecutionsLoading } from "@/features/executions/components/executions";
 
-const Page=async()=>{
-    await requireAuth();
-    return <p>executions</p>
-}
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+const Page = async ({ searchParams }: Props) => {
+  await requireAuth();
+
+  const params = await executionsParamsLoader(searchParams);
+  prefetchExecutions(params);
+
+  return (
+    <ExecutionsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<ExecutionsError />}>
+          <Suspense fallback={<ExecutionsLoading/>}>
+            <ExecutionsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </ExecutionsContainer>
+  );
+};
 
 export default Page;
